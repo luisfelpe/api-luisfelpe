8a085cb9c3d932c848cefd46147d11256c51fce1
Luis Felipe Teobaldo <luis.teobaldo09@aluno.ifce.edu.br>
2025-10-21 11:32:50 +0000

    revamp

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 from typing import List
+from http import HTTPStatus
+from .schema import CreateReceita,Receita
 
 app = FastAPI(title="Api do Felps")
-
-
-class CreateReceita(BaseModel):
-    nome: str
-    ingredientes: List[str]
-    modo_de_preparo: str
-
-
-class Receita(BaseModel):
-    id: int
-    nome: str
-    ingredientes: List[str]
-    modo_de_preparo: str
 
 
 receitas: List[Receita] = []
@@ -26,22 +14,22 @@ def hello():
     return {"title": "Livro de Receitas"}
 
 
-@app.get("/receitas")
+@app.get("/receitas", response_model=List[Receita], status_code=HTTPStatus.OK)
 def get_todas_receitas():
     if len(receitas) == 0:
-        return {"mensagem": "Sobrou Nada Pro Betinha"}
+        raise HTTPException(status_code=HTTPException.NOT_FOUND, detail="Sobrou Nada Pro Betinha")
     return receitas
 
 
-@app.get("/receitas/{receita}")
+@app.get("/receitas/{receita}", response_model=List[Receita], status_code=HTTPStatus.OK)
 def get_receita(receita: str):
     for i in receitas:
         if i.nome == receita:
             return i
-    return {"mensagem": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPException.NOT_FOUND, detail="Receita não encontrada")
 
 
-@app.post("/receitas")
+@app.post("/receitas", response_model=List[Receita], status_code=HTTPStatus.CREATED)
 def criar_receita(dados: CreateReceita):
     id = len(receitas) + 1
     nova_receita = Receita(
@@ -52,22 +40,22 @@ def criar_receita(dados: CreateReceita):
     )
     for i in receitas:
         if i.nome.upper() == nova_receita.nome.upper():
-            return "mensagem: Receita já criada"
+            raise HTTPException(status_code=HTTPException.BAD_REQUEST, detail="Receita já criada")
     if 2 > len(nova_receita.nome) > 50:
-        return {"mensagem": "Fora do Limite"}
+        raise HTTPException(status_code=HTTPException.BAD_REQUEST, detail="Fora do Limite")
     receitas.append(nova_receita)
     return nova_receita
 
 
-@app.get("/receitas/id/{id}")
+@app.get("/receitas/id/{id}", response_model=List[Receita], status_code=HTTPStatus.OK)
 def get_receita_por_id(id: int):
     for i in receitas:
         if i.id == id:
             return i
-    return {"mensagem": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPException.NOT_FOUND, detail="Receita não encontrada")
 
 
-@app.put("/receitas/{id}")
+@app.put("/receitas/{id}", response_model=List[Receita], status_code=HTTPStatus.OK)
 def uptade_receita(id: int, dados: CreateReceita):
     for i in range(len(receitas)):
         if receitas[i].id == id:
@@ -79,21 +67,21 @@ def uptade_receita(id: int, dados: CreateReceita):
             )
             for j in range(len(receitas)):
                 if receitas[j].nome.upper() == receita_atualizada.nome.upper():
-                    return {"mensagem: Já existe uma receita com esse nome"}
+                    raise HTTPException(status_code=HTTPException.CONFLICT, detail="Já existe uma receita com esse nome")
             if receita_atualizada.nome == "":
-                return {"mensagem": "insira um nome"}
+                raise HTTPException(status_code=HTTPException.BAD_REQUEST, detail="insira um nome")
             receitas[i] = receita_atualizada
             return receita_atualizada
-    return {"mensagem: Receita não encontrada"}
+    raise HTTPException(status_code=HTTPException.NOT_FOUND, detail="Receita não encontrada")
 
 
-@app.delete("/receitas/{id}")
+@app.delete("/receitas/{id}", response_model=List[Receita], status_code=HTTPStatus.OK)
 def deletar_receia(id: int):
     if len(receitas) == 0:
-        return {"mensagem": "Não há receitas"}
+        raise HTTPException(status_code=HTTPException.NOT_FOUND, detail="Não há Receitas")
     for i in range(len(receitas)):
         if receitas[i].id == id:
             m = receitas[i].nome
             receitas.pop(i)
             return {"mensagem": "A Receita " + m + " Foi Deletada:"}
-    return {"mensagem": "Receita não encontrada"}
+    raise HTTPException(status_code=HTTPException.NOT_FOUND, detail="Receita não encontrada")
