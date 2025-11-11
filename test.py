66900ed439a81cd8bc6b2c9b7d3c700aa504b2d1
Luis Felipe Teobaldo <luis.teobaldo09@aluno.ifce.edu.br>
2025-11-11 11:29:37 +0000

    .

---
+++ test.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+from models import User, tabela_registro
+
+app = FastAPI(title='API de teste')
+
+engine = create_engine("sqlite:///:memory:", echo=False)
+
+tabela_registro.metadata.create_all(engine)
+
+with Session(engine) as session:
+    Felps = User(
+        username="Felipe", password="4002-8922", email="darkwraith@opmail.com"
+    )
+    session.add(Felps)
+    session.commit()
+    session.refresh(Felps)
+
+print("Dados do usuario:", Felps)
+print("ID:", Felps.id)
+print("Criado em:", Felps.created_at)
