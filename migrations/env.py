c1d9305fa08f6ce2b2b8ef12a89edf1f983ecf42
Luis Felipe Teobaldo <luis.teobaldo09@aluno.ifce.edu.br>
2025-11-18 11:49:20 +0000

    .

--- migrations/env.py
+++ migrations/env.py
@@ -9,7 +9,7 @@ from models import  tabela_registro
 from settings import Settings
 print("DATABASE_URL", Settings().DATABASE_URL)
 config = context.config
-config.set_main_option('sqlalchemy_url', Settings().DATABASE_URL)
+config.set_main_option('sqlalchemy.url', Settings().DATABASE_URL)
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
